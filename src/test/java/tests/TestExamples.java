5ed18709ca8aaf6af5b12f93d6b4ba4163afecdb
Mehmet <mehmetdolasan10@gmail.com>
2024-04-11 00:11:50 +0300

    run a get request, store response and print response data

--- src/test/java/tests/TestExamples.java
+++ src/test/java/tests/TestExamples.java
@@ -1,6 +1,9 @@
 package tests;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,9 +11,9 @@ import org.testng.annotations.Test;
 public class TestExamples {
 
     @Test
-    public void test_1(){
+    public void test_01(){
 
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        Response response = get("https://reqres.in/api/users?page=2");
 
         System.out.println(response.getStatusCode());
         System.out.println(response.getTime());
@@ -21,6 +24,18 @@ public class TestExamples {
         int statusCode = response.getStatusCode();
 
         Assert.assertEquals(statusCode,200);
+
+    }
+
+    @Test
+    public void test_02(){
+        baseURI = "https://reqres.in/api";
+        given().
+                get("/users?page=2").
+                then().
+                statusCode(200).
+                body("data.id[1]",equalTo(8)).
+                log().all();
 
     }
 
